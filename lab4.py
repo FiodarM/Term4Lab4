a0106b1ff75e59f7ac898eba30756a9f94403bfe
FiodarM <fyodormorozko95@gmail.com>
2015-04-04 15:16:13 +0300

    edits

--- lab4.py
+++ lab4.py
@@ -6,9 +6,12 @@ from scipy.integrate import odeint
 
 f = lambda y, x: x**2 - y
 x = np.linspace(0, 5.)
-y = odeint(f, 2., x)
+y_adm = odesolve_adams(f, 2., x)
+y_rk = odesolve(f, 2., x)
 
 fig = plt.figure(tight_layout=True)
 ax = fig.add_subplot(111)
-ax.plot(x, y)
+ax.plot(x, y_adm, label='Adams method')
+ax.plot(x, y_rk, label='Runge - Kutta method')
+ax.legend(loc='best')
 plt.show()
--- ode.py
+++ ode.py
@@ -3,15 +3,61 @@ __author__ = 'fiodar'
 import numpy as np
 
 
-def odesolve_adams(f, y0, sequence, tol):
-    y = np.array([y0], dtype=float)
+def odesolve_adams(f, y0, sequence, tol=1e-3):
+    y = odesolve(f, y0, sequence[:4], tol)
     h = sequence[1] - sequence[0]
-    predict_y = lambda x: y + h / 24. * (55 * f(x) - 59 * f(x - h) + 37 * f(x - 2 * h) + 9 * f(x - 3 * h))
-    correct_y = lambda x: y + h / 24. * (9 * f(x + h) - 19 * f(x) - 5 * f(x - h) + f(x - 2 * h))
+    predict_y = lambda y, x: y + h / 24. * (
+    55 * f(y, x) - 59 * f(y, x - h) + 37 * f(y, x - 2 * h) + 9 * f(y, x - 3 * h))
+    correct_y = lambda y, x: y + h / 24. * (9 * f(y, x) - 19 * f(y, x - h) - 5 * f(y, x - 2 * h) + f(y, x - 3 * h))
 
-    for x in sequence[3:]:
-        y = np.column_stack((y, predict_y(x)))
-        while np.abs(correct_y(x) - y[-1]).any() > tol:
-            y[-1] = correct_y(x)
+    for x in sequence[4:]:
+        y = np.append(y, predict_y(y[-1], x))
+        # while np.greater(np.abs(correct_y(y[-1], x) - y[-1]), tol).any():
+        y[-1] = correct_y(y[-1], x)
 
     return y
+
+
+def odesolve(f, y0, sequence, tolerance=1e-2):
+    sequence = np.append([], sequence)
+    t = t0 = sequence[0]
+    y = np.array(y0)
+    ytable = np.array([y0])
+    dt = (sequence[-1] - t0) * tolerance
+    steps = 0
+
+    k1 = lambda: f(y, t)
+    k2 = lambda dx: f(y + k1() * dx / 2., t + dx / 2.)
+    k3 = lambda dx: f(y + k2(dx) * dx / 2., t + dx / 2.)
+    k4 = lambda dt: f(y + k3(dt) * dt, t + dt)
+    next_y = lambda dt: y + dt / 6. * (k1() + 2 * k2(dt) + 2 * k3(dt) + k4(dt))
+    rest = lambda dt: np.abs(np.array(next_y(dt) - next_y(dt / 2.))) / 15.
+
+    for end in sequence[1:]:
+
+        while abs(t - t0) < abs(end - t0):
+
+            if np.equal(rest(dt), rest(dt / 2.)).all():
+                dt = end - t
+
+            elif np.greater_equal(rest(dt), tolerance * np.abs(next_y(dt / 2.))).any():
+                while np.greater_equal(rest(dt), tolerance * np.abs(next_y(dt / 2.))).any() \
+                        and abs(dt) > 1e-6:
+                    dt *= 0.5
+
+            else:
+                while np.less(rest(dt), tolerance * np.abs(next_y(dt / 2.))).all():
+                    dt *= 1.2
+                    if abs(t + dt - t0) > abs(end - t0):
+                        dt = end - t
+                        break
+
+            y = next_y(dt)
+            t += dt
+            steps += 1
+
+        t0 = t
+
+        ytable = np.append(ytable, [y], axis=0)
+
+    return ytable
